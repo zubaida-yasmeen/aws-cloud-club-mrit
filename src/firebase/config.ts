99f930cb3712b00def529adf94053fef0bd57231
Zubeda Yasmeen <zubedayasmeen2316@gmail.com>
2026-04-08 15:30:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-282199874-4f1e8",
+  "appId": "1:347868393968:web:a3d5602ab0587dfc28ed35",
+  "apiKey": "AIzaSyBZML-7oz_f-nYxE9-SDt_XvANsROWKme4",
+  "authDomain": "studio-282199874-4f1e8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "347868393968"
+};
